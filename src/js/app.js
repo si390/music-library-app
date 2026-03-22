9a7d3b8a507a02ffe312b93f35eb77c1d8237ef2
si390 <seb163@hotmail.com>
2026-03-21 20:04:06 -0500

    search

--- src/js/app.js
+++ src/js/app.js
@@ -2,16 +2,17 @@ import { getSongs } from './data.js';
 import { renderSongs } from './ui.js';
 import { filterSongs } from './filter.js';
 import { playSong, pauseSong } from './player.js';
+import { getFavorites } from './storage.js';
 import '../css/variables.css';
 import '../css/styles.css';
 
 document.addEventListener('DOMContentLoaded', async () => {
   const resultsContainer = document.getElementById('results');
   const searchInput = document.getElementById('searchInput');
-
+  let showingFavorites = false;
   const playBtn = document.getElementById('playBtn');
   const pauseBtn = document.getElementById('pauseBtn');
-
+  const favoritesBtn = document.getElementById('favoritesBtn');
   const songs = await getSongs();
 
   // Render inicial
@@ -21,7 +22,8 @@ document.addEventListener('DOMContentLoaded', async () => {
   searchInput.addEventListener('input', (e) => {
     const query = e.target.value;
 
-    const filteredSongs = filterSongs(songs, query);
+    const source = showingFavorites ? getFavorites() : songs;
+    const filteredSongs = filterSongs(source, query);
 
     renderSongs(filteredSongs, resultsContainer);
   });
@@ -29,4 +31,16 @@ document.addEventListener('DOMContentLoaded', async () => {
    // 🎧 Controles del player
   playBtn.addEventListener('click', playSong);
   pauseBtn.addEventListener('click', pauseSong);
+  favoritesBtn.addEventListener('click', () => {
+    showingFavorites = !showingFavorites;
+
+    if (showingFavorites) {
+      const favorites = getFavorites();
+      renderSongs(favorites, resultsContainer);
+      favoritesBtn.textContent = '🏠'; // volver a inicio
+    } else {
+      renderSongs(songs, resultsContainer);
+      favoritesBtn.textContent = '❤️';
+    }
+  });
 });
